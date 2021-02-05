151f5730fcf6208d0bc29d1fa7c2f958d6e6324b
nmparfait <mbamu.developer@gmail.com>
2021-02-05 14:22:48 +0000

    cleanning the code and make it easy

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from art import logo
 print(logo)
 
-print("Welcome to the Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-from random import randint
-
-#Choosing a random number between 1 and 100.
-answer = randint(1, 100)
-print(f"Psssss the correct answer is {answer}")
-
+#global variables 
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
-
-#Make a function to set difficulty.
-def check_answer(guess, answer):
-  if guess > answer:
-    print("Too high.")
-  elif guess < answer:
-    print("Too low.")
-  else:
-    print(f"You got it! The answer was {answer}.")
 
 #check the difficulty
 def set_difficulty():
@@ -29,15 +13,37 @@ def set_difficulty():
   else:
     return HARD_LEVEL_TURNS
 
+#Make a function to check the answer
+def check_answer(guess, answer):
+  if guess > answer:
+    print("Too high.")
+  elif guess < answer:
+    print("Too low.")
+  else:
+    print(f"You got it! The answer was {answer}.")
+#Choosing a random number between 1 and 100.
+def game():
+  #Welcome to the game 
+  print("Welcome to the Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+
+  from random import randint
+  answer = randint(1, 100)
+  print(f"Psssss the correct answer is {answer}")
+  turns = set_difficulty()
+  print(f"You have {turns} attempts")
+
+  #Repeat the guessing functionnality to see if they get it wrong
+  guess = 0
+  while guess != answer:
+    guess = int(input("Make a guess "))
+    check_answer(guess, answer)
+
+game()
 
 
 
-#Let user guess a number
-guess = int(input("Make a guess: "))
-turns = 0
 
-set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number.")
 
 #Function to check the user's guess against actual answer. 
 
